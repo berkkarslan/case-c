8151efaf08033d9876c6d5f71ee5b6f8f59c975d
Berk Arslan <berkarslan97@gmail.com>
2022-09-16 16:17:56 +0300

    added FormTable component

---
+++ src/components/FormTable.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+interface Props {
+  data: MyForm[]
+}
+const FormTable: React.FC<Props> = ({ data }) => {
+  const navigate = useNavigate()
+  const [search, setSearch] = useState('')
+  const filteredData = data.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()))
+
+  const row = (item: MyForm) => (
+    <tr>
+      <td>{item.name}</td>
+      <td>{item.description}</td>
+      <td>{item.createdAt}</td>
+      <td>
+        <button onClick={() => navigate(`forms/${item.name}`)} className='btn btn-primary'>
+          Details
+        </button>
+      </td>
+    </tr>
+  )
+
+  return (
+    <>
+      <div className='d-flex justify-content-between pt-3'>
+        <button className='btn btn-primary'>Create New Form</button>
+        <div>
+          <input
+            type='text'
+            id='search'
+            className='form-control'
+            placeholder='Search by Name'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+      </div>
+      <table className='table table-striped w-100 mt-3'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Created At</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredData.length > 0 ? (
+            filteredData.map((item) => row(item))
+          ) : (
+            <tr>
+              <td colSpan={4}>No data</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default FormTable
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,25 @@
 import React from 'react'
+import FormTable from '../components/FormTable'
 const Home: React.FC = () => {
-  return <div>home</div>
+  const data: MyForm[] = [
+    {
+      name: 'Test form',
+      description: 'Uye bilgi formu',
+      createdAt: '2017-01-08',
+      fields: [
+        { required: true, name: 'Ad', dataType: 'STRING' },
+        { required: true, name: 'Soyad', dataType: 'STRING' },
+        { required: false, name: 'Yaş', dataType: 'NUMBER' },
+      ],
+    },
+  ]
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <FormTable data={data} />
+    </div>
+  )
 }
 
 export default Home
